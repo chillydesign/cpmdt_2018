2800386254ba7f065326ad46ba39a302b15b326a
Charles Harvey <harvey.charles@gmail.com>
2018-12-20 17:28:04 +0000

    add new page for music theatre and dance

--- js/app.js
+++ js/app.js
@@ -111,10 +111,11 @@ $no_people.on('change', function(e) {
 
 
 
-
+if (typeof google != 'undefined') {
 // locations map
 // locations map
 var map_theme=[{featureType:"all",elementType:"all",stylers:[{visibility:"off"}]},{featureType:"administrative",elementType:"all",stylers:[{visibility:"on"}]},{featureType:"landscape",elementType:"all",stylers:[{visibility:"on"}]},{featureType:"poi",elementType:"all",stylers:[{visibility:"on"}]},{featureType:"road",elementType:"all",stylers:[{visibility:"on"}]},{featureType:"transit",elementType:"all",stylers:[{visibility:"on"}]},{featureType:"water",elementType:"all",stylers:[{visibility:"on"}]},{featureType:"all",elementType:"geometry.fill",stylers:[{weight:"2.00"}]},{featureType:"all",elementType:"geometry.stroke",stylers:[{color:"#9c9c9c"}]},{featureType:"all",elementType:"labels.text",stylers:[{visibility:"on"}]},{featureType:"landscape",elementType:"all",stylers:[{color:"#f2f2f2"}]},{featureType:"landscape",elementType:"geometry.fill",stylers:[{color:"#ffffff"}]},{featureType:"landscape.man_made",elementType:"geometry.fill",stylers:[{color:"#ffffff"}]},{featureType:"poi",elementType:"all",stylers:[{visibility:"off"}]},{featureType:"road",elementType:"all",stylers:[{saturation:-100},{lightness:45}]},{featureType:"road",elementType:"geometry.fill",stylers:[{color:"#eeeeee"}]},{featureType:"road",elementType:"labels.text.fill",stylers:[{color:"#7b7b7b"}]},{featureType:"road",elementType:"labels.text.stroke",stylers:[{color:"#ffffff"}]},{featureType:"road.highway",elementType:"all",stylers:[{visibility:"simplified"}]},{featureType:"road.arterial",elementType:"labels.icon",stylers:[{visibility:"off"}]},{featureType:"transit",elementType:"all",stylers:[{visibility:"off"}]},{featureType:"water",elementType:"all",stylers:[{color:"#46bcec"},{visibility:"on"}]},{featureType:"water",elementType:"geometry.fill",stylers:[{color:"#c8d7d4"}]},{featureType:"water",elementType:"labels.text.fill",stylers:[{color:"#070707"}]},{featureType:"water",elementType:"labels.text.stroke",stylers:[{color:"#ffffff"}]}];
+
 var map_options = {
     zoom: 13,
     mapTypeControl: true,
@@ -126,7 +127,7 @@ var map_options = {
 };
 
 
-if (typeof locations_for_map != 'undefined'){
+if (typeof locations_for_map != 'undefined' ){
 
 
 
@@ -174,6 +175,8 @@ if (typeof single_location_for_map !== 'undefined') {
         single_map.setZoom(16);
 
 }
+
+};  // end of google defined
 
 
 function addPointToMap(map,  location, bounds, infowindow, markers, custom_icon ) {
@@ -332,6 +335,18 @@ function addPointToMap(map,  location, bounds, infowindow, markers, custom_icon 
         //     $upper_age  = agesplit[1];
         // }
 
+
+
+
+    };
+
+    if (typeof course_category != 'undefined') {
+        $search_checks.each( function() {
+            var $this = $(this);
+            if ( $this.data('field') == 'category'  &&  $this.val() == course_category ) {
+                this.click();
+            }
+        } )
 
     }
 
--- page-search.php
+++ page-search.php
@@ -1,16 +1,10 @@
 <?php /* Template Name: Search.page */ get_header(); ?>
 
+
 <!-- Search bar -->
 <?php get_template_part('programme.search'); ?>
-<p class="container" id="course_size"></p>
-<div id="courses_container"></div>
-<script type="text/javascript">
-	var search_url = '<?php echo home_url(); ?>/api/v1/';
-</script>
-<script id="courses_template" type="x-underscore">
-	<?php echo file_get_contents(dirname(__FILE__) . '/templates/courses.underscore'); ?>
-</script>
 
+<?php get_template_part( 'coursesearch' ); ?>
 
 
 
