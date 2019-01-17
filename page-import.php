49fa2fccac104b5bf7c06668f25a3d906859d229
Charles Harvey <harvey.charles@gmail.com>
2019-01-17 14:13:19 +0000

    add improts to page import template

--- page-import.php
+++ page-import.php
@@ -1,7 +1,7 @@
 <?php /* Template Name: Import template */ get_header(); ?>
 
 
-<?php // import_old_bookings_data(); ?>
-<?php // import_old_program_data(); ?>
+<?php  import_old_bookings_data(); ?>
+<?php  import_old_program_data(); ?>
 
 <?php get_footer(); ?>
