07b0ddc71b0545d9b4da7c6c12c6d3c9a3dbb24a
Charles Harvey <harvey.charles@gmail.com>
2019-01-19 08:44:58 +0000

    dito

--- front-page.php
+++ front-page.php
@@ -23,7 +23,7 @@
                     <div class="news_slider">
 					<?php foreach ( $home_page_news as $news_item) :
                         $post = $news_item['post'];
-                        $status = $news_item->post_status;
+                        $status = $post->post_status;
                         if ($status == 'publish') :
                         $excerpt = get_field('post_excerptCF', $post->ID);
 						$category = get_the_category( $post->ID );
