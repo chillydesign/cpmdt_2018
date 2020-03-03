b46077b6becaa38d51b8d3e11a8103bc820d2a44
Charles Harvey <harvey.charles@gmail.com>
2020-03-03 10:29:53 +0100

    remove search by title only

--- functions.php
+++ functions.php
@@ -666,43 +666,43 @@ function remove_events_from_search( $wp_query ) {
 }
 
 
-
-add_filter( 'posts_search', '__search_by_title_only', 500, 2 );
-function __search_by_title_only( $search, $wp_query ) {
-    global $wpdb;
-
-
-    if ( is_admin() ) {
-        return $search;
-    } else {
-
-        if ( empty( $search ) )
-        return $search; // skip processing - no search term in query
-
-        $q = $wp_query->query_vars;
-        $n = ! empty( $q['exact'] ) ? '' : '%';
-
-        $search =
-        $searchand = '';
-
-        foreach ( (array) $q['search_terms'] as $term ) {
-            $term = esc_sql( $wpdb->esc_like( $term ) );
-            $search .= "{$searchand}($wpdb->posts.post_title LIKE '{$n}{$term}{$n}')";
-            $searchand = ' AND ';
-        }
-
-        if ( ! empty( $search ) ) {
-            $search = " AND ({$search}) ";
-            if ( ! is_user_logged_in() )
-            $search .= " AND ($wpdb->posts.post_password = '') ";
-        }
-
-        return $search;
-
-    } // end if not admin pages
+// REMOVED SEARCH BY TITLE ONLY
+// add_filter( 'posts_search', '__search_by_title_only', 500, 2 );
+// function __search_by_title_only( $search, $wp_query ) {
+//     global $wpdb;
 
 
-}
+//     if ( is_admin() ) {
+//         return $search;
+//     } else {
+
+//         if ( empty( $search ) )
+//         return $search; // skip processing - no search term in query
+
+//         $q = $wp_query->query_vars;
+//         $n = ! empty( $q['exact'] ) ? '' : '%';
+
+//         $search =
+//         $searchand = '';
+
+//         foreach ( (array) $q['search_terms'] as $term ) {
+//             $term = esc_sql( $wpdb->esc_like( $term ) );
+//             $search .= "{$searchand}($wpdb->posts.post_title LIKE '{$n}{$term}{$n}')";
+//             $searchand = ' AND ';
+//         }
+
+//         if ( ! empty( $search ) ) {
+//             $search = " AND ({$search}) ";
+//             if ( ! is_user_logged_in() )
+//             $search .= " AND ($wpdb->posts.post_password = '') ";
+//         }
+
+//         return $search;
+
+//     } // end if not admin pages
+
+
+// }
 
 
 
