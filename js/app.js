485e326af930196481f856056849ee6458fc95e2
Charles Harvey <harvey.charles@gmail.com>
2020-12-09 13:02:36 +0000

    chagned exepmt option text

--- js/app.js
+++ js/app.js
@@ -755,7 +755,13 @@
 
                     if ($course_id == 9285 || $course_id == 9389) {
                         // if course is musique theatre
-                        var $exempt_option = '<option value="Exempt de formation musicale" >Cours pluridisciplinaire Musique-théâtre : exempt de FM</option>';
+
+                        // var $exempt_option = '<option value="Exempt de formation musicale" >Cours pluridisciplinaire Musique-théâtre : exempt de FM</option>';
+
+                        var $exempt_option = '<option value="Exempt de formation musicale" >Exempt de formation musicale (musique-théâtre uniquement)</option>';
+
+
+
 
                         var $formation_musicale_field = $('#formation_musicale');
 
