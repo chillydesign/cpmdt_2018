ab908b1e93ecc4361a7b9fa9de00b455866580bb
Charles Harvey <harvey.charles@gmail.com>
2021-01-19 09:35:31 +0000

    add cluster images

--- js/app.js
+++ js/app.js
@@ -209,7 +209,10 @@
 
                 }
 
-                var n_cluster = new MarkerClusterer(n_location_map, n_location_markers);
+                var n_cluster = new MarkerClusterer(n_location_map, n_location_markers, {
+                    imagePath:
+                        theme_directory + '/assets/',
+                });
 
 
 
