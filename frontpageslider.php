c2d95013892f1ebdd53b63385812b45145373710
Charles Harvey <harvey.charles@gmail.com>
2021-03-03 17:44:23 +0000

    marker front page slider images full size (bad idea)

--- frontpageslider.php
+++ frontpageslider.php
@@ -1,12 +1,12 @@
 <?php $gallery = get_field('slider'); ?>
 
-<?php if ($gallery): ?>
+<?php if ($gallery) : ?>
 
-<?php if( count( $gallery ) > 1) : ?>
+  <?php if (count($gallery) > 1) : ?>
     <ul class="front-page_slider">
-        <?php foreach ($gallery as $image) : ?>
-            <li class="frontpage_image" style="background-image:url('<?php echo $image['sizes']['large']; ?>');">  </li>
-        <?php endforeach; ?>
+      <?php foreach ($gallery as $image) : ?>
+        <li class="frontpage_image" style="background-image:url('<?php echo $image['url']; ?>');"> </li>
+      <?php endforeach; ?>
     </ul>
     <div class="container">
       <div class="hometitle">
@@ -14,15 +14,15 @@
         <h2>Trois arts de la scène réunis dans une même école</h2>
       </div>
     </div>
-<?php else: ?>
-  <div class="frontpage_image"style="background-image:url('<?php echo $gallery[0]['sizes']['large']; ?>');">
-    <div class="container">
-      <div class="hometitle">
-        <h1>Musique, Danse, Théâtre</h1>
-        <h2>Trois arts de la scène réunis dans une même école</h2>
+  <?php else : ?>
+    <div class="frontpage_image" style="background-image:url('<?php echo $gallery[0]['url']; ?>');">
+      <div class="container">
+        <div class="hometitle">
+          <h1>Musique, Danse, Théâtre</h1>
+          <h2>Trois arts de la scène réunis dans une même école</h2>
+        </div>
       </div>
     </div>
-  </div>
-<?php endif; ?>
+  <?php endif; ?>
 
 <?php endif; ?>
